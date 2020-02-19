5b5e554b654d259b4ba7b1e1a1dda88b5634bfa5
Alpar <alpar1899@gmail.com>
2020-02-19 22:09:16 +0100

    Entitiy framework and Identity added

---
+++ Application/Validators/ValidatorExtentions.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+
+namespace Application.Validators
+{
+    public static class ValidatorExtentions
+    {
+        public static IRuleBuilder<T, string> Password<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            var option = ruleBuilder
+                .NotEmpty()
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters")
+                .Matches("[A-Z]").WithMessage("Password must contain at least 1 uppercase letter")
+                .Matches("[a-z]").WithMessage("Password must contain at least 1 lowercase letter")
+                .Matches("[0-9]").WithMessage("Password must contain at least 1 number")
+                .Matches("[^a-zA-Z1-9]").WithMessage("Password must contain at least 1 non alphanumeric");
+        
+            return option;
+        }
+    }
+}
